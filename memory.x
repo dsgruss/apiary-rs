be9b4ba0d20d0e35e4d8e450f615bd56483d91a2
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-06-17 11:32:36 -0700

    Add initial eth code and packages to manifest

--- memory.x
+++ memory.x
@@ -1,10 +1,10 @@
 MEMORY
 {
-  /* NOTE 1 K = 1 KiBi = 1024 bytes */
-  /* TODO Adjust these memory regions to match your device memory layout */
-  /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
-  FLASH : ORIGIN = 0x00000000, LENGTH = 256K
-  RAM : ORIGIN = 0x20000000, LENGTH = 64K
+  FLASH (rx)      : ORIGIN = 0x8000000, LENGTH = 2048K
+  RAM (xrw)       : ORIGIN = 0x20000000, LENGTH = 112K
+  RAM2 (xrw)      : ORIGIN = 0x2001C000, LENGTH = 16K
+  RAM3 (xrw)      : ORIGIN = 0x20020000, LENGTH = 64K
+  CCMRAM (rw)     : ORIGIN = 0x10000000, LENGTH = 64K
 }
 
 /* This is where the call stack will be allocated. */
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,248 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+// extern crate panic_itm;
+use panic_semihosting as _;
 
 use cortex_m::asm;
-use cortex_m_rt::entry;
+use cortex_m_rt::{entry, exception};
+use stm32_eth::{
+    hal::gpio::GpioExt,
+    hal::rcc::RccExt,
+    stm32::{interrupt, CorePeripherals, Peripherals, SYST},
+};
+
+use core::cell::RefCell;
+use cortex_m::interrupt::Mutex;
+
+use core::fmt::Write;
+use cortex_m_semihosting::hio;
+
+use fugit::RateExtU32;
+use log::{Level, LevelFilter, Metadata, Record};
+use smoltcp::iface::{Interface, InterfaceBuilder, NeighborCache, Routes};
+use smoltcp::socket::{TcpSocket, TcpSocketBuffer, Dhcpv4Event, Dhcpv4Socket};
+use smoltcp::time::Instant;
+use smoltcp::wire::{EthernetAddress, IpCidr, Ipv4Cidr, Ipv4Address};
+use smoltcp::phy::Device;
+
+use stm32_eth::{EthPins, RingEntry};
+
+static mut LOGGER: HioLogger = HioLogger {};
+
+struct HioLogger {}
+
+impl log::Log for HioLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Trace
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            let mut stdout = hio::hstdout().unwrap();
+            writeln!(stdout, "{} - {}", record.level(), record.args()).unwrap();
+        }
+    }
+    fn flush(&self) {}
+}
+
+const SRC_MAC: [u8; 6] = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
+
+static TIME: Mutex<RefCell<u64>> = Mutex::new(RefCell::new(0));
+static ETH_PENDING: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    unsafe {
+        log::set_logger(&LOGGER).unwrap();
+    }
+    log::set_max_level(LevelFilter::Info);
+
+    let mut stdout = hio::hstdout().unwrap();
+
+    let p = Peripherals::take().unwrap();
+    let mut cp = CorePeripherals::take().unwrap();
+
+    let rcc = p.RCC.constrain();
+    // HCLK must be at least 25MHz to use the ethernet peripheral
+    let clocks = rcc.cfgr.sysclk(32.MHz()).hclk(32.MHz()).freeze();
+
+    setup_systick(&mut cp.SYST);
+
+    writeln!(stdout, "Enabling ethernet...").unwrap();
+    let gpioa = p.GPIOA.split();
+    let gpiob = p.GPIOB.split();
+    let gpioc = p.GPIOC.split();
+    let gpiog = p.GPIOG.split();
+
+    let eth_pins = EthPins {
+        ref_clk: gpioa.pa1,
+        crs: gpioa.pa7,
+        tx_en: gpiog.pg11,
+        tx_d0: gpiog.pg13,
+        tx_d1: gpiob.pb13,
+        rx_d0: gpioc.pc4,
+        rx_d1: gpioc.pc5,
+    };
+
+    let mut rx_ring: [RingEntry<_>; 8] = Default::default();
+    let mut tx_ring: [RingEntry<_>; 2] = Default::default();
+    let (mut eth_dma, _eth_mac) = stm32_eth::new(
+        p.ETHERNET_MAC,
+        p.ETHERNET_MMC,
+        p.ETHERNET_DMA,
+        &mut rx_ring[..],
+        &mut tx_ring[..],
+        clocks,
+        eth_pins,
+    )
+    .unwrap();
+    eth_dma.enable_interrupt();
+
+    let ip_addr = IpCidr::new(Ipv4Address::UNSPECIFIED.into(), 0);
+    let mut ip_addrs = [ip_addr];
+    let mut neighbor_storage = [None; 16];
+    let neighbor_cache = NeighborCache::new(&mut neighbor_storage[..]);
+    let mut routes_storage = [None; 1];
+    let routes = Routes::new(&mut routes_storage[..]);
+    let ethernet_addr = EthernetAddress(SRC_MAC);
+
+    let mut sockets: [_; 2] = Default::default();
+    let mut iface = InterfaceBuilder::new(&mut eth_dma, &mut sockets[..])
+        .hardware_addr(ethernet_addr.into())
+        .ip_addrs(&mut ip_addrs[..])
+        .routes(routes)
+        .neighbor_cache(neighbor_cache)
+        .finalize();
+
+    let dhcp_socket = Dhcpv4Socket::new();
+    let dhcp_handle = iface.add_socket(dhcp_socket);
 
     loop {
-        // your code goes here
+        let time: u64 = cortex_m::interrupt::free(|cs| *TIME.borrow(cs).borrow());
+        if let Err(e) = iface.poll(Instant::from_millis(time as i64)) {
+            writeln!(stdout, "Error: {:?}", e).unwrap();
+        }
+
+        let event = iface.get_socket::<Dhcpv4Socket>(dhcp_handle).poll();
+        match event {
+            None => {}
+            Some(Dhcpv4Event::Configured(config)) => {
+                writeln!(stdout, "DHCP config acquired!").unwrap();
+
+                writeln!(stdout, "IP address:      {}", config.address).unwrap();
+                set_ipv4_addr(&mut iface, config.address);
+
+                if let Some(router) = config.router {
+                    writeln!(stdout, "Default gateway: {}", router).unwrap();
+                    iface.routes_mut().add_default_ipv4_route(router).unwrap();
+                } else {
+                    writeln!(stdout, "Default gateway: None").unwrap();
+                    iface.routes_mut().remove_default_ipv4_route();
+                }
+
+                for (i, s) in config.dns_servers.iter().enumerate() {
+                    if let Some(s) = s {
+                        writeln!(stdout, "DNS server {}:    {}", i, s).unwrap();
+                    }
+                }
+                break;
+            }
+            Some(Dhcpv4Event::Deconfigured) => {
+                writeln!(stdout, "DHCP lost config!").unwrap();
+                set_ipv4_addr(&mut iface, Ipv4Cidr::new(Ipv4Address::UNSPECIFIED, 0));
+                iface.routes_mut().remove_default_ipv4_route();
+            }
+        }
+
+        asm::wfi();
     }
+
+    let mut server_rx_buffer = [0; 2048];
+    let mut server_tx_buffer = [0; 2048];
+    let server_socket = TcpSocket::new(
+        TcpSocketBuffer::new(&mut server_rx_buffer[..]),
+        TcpSocketBuffer::new(&mut server_tx_buffer[..]),
+    );
+    let server_handle = iface.add_socket(server_socket);
+
+    writeln!(stdout, "Socket created and ready!").unwrap();
+    loop {
+        let time: u64 = cortex_m::interrupt::free(|cs| *TIME.borrow(cs).borrow());
+        cortex_m::interrupt::free(|cs| {
+            let mut eth_pending = ETH_PENDING.borrow(cs).borrow_mut();
+            *eth_pending = false;
+        });
+        match iface.poll(Instant::from_millis(time as i64)) {
+            Ok(true) => {
+                let socket = iface.get_socket::<TcpSocket>(server_handle);
+                if !socket.is_open() {
+                    socket
+                        .listen(80)
+                        .or_else(|e| writeln!(stdout, "TCP listen error: {:?}", e))
+                        .unwrap();
+                }
+
+                if socket.can_send() {
+                    write!(socket, "hello\n")
+                        .map(|_| {
+                            socket.close();
+                        })
+                        .or_else(|e| writeln!(stdout, "TCP send error: {:?}", e))
+                        .unwrap();
+                }
+            }
+            Ok(false) => {
+                // Sleep if no ethernet work is pending
+                cortex_m::interrupt::free(|cs| {
+                    let eth_pending = ETH_PENDING.borrow(cs).borrow_mut();
+                    if !*eth_pending {
+                        asm::wfi();
+                        // Awaken by interrupt
+                    }
+                });
+            }
+            Err(e) =>
+            // Ignore malformed packets
+            {
+                writeln!(stdout, "Error: {:?}", e).unwrap()
+            }
+        }
+    }
+}
+
+fn set_ipv4_addr<DeviceT>(iface: &mut Interface<'_, DeviceT>, cidr: Ipv4Cidr)
+where
+    DeviceT: for<'d> Device<'d>,
+{
+    iface.update_ip_addrs(|addrs| {
+        let dest = addrs.iter_mut().next().unwrap();
+        *dest = IpCidr::Ipv4(cidr);
+    });
+}
+
+fn setup_systick(syst: &mut SYST) {
+    syst.set_reload(SYST::get_ticks_per_10ms() / 10);
+    syst.enable_counter();
+    syst.enable_interrupt();
+}
+
+#[exception]
+fn SysTick() {
+    cortex_m::interrupt::free(|cs| {
+        let mut time = TIME.borrow(cs).borrow_mut();
+        *time += 1;
+    })
+}
+
+#[interrupt]
+fn ETH() {
+    cortex_m::interrupt::free(|cs| {
+        let mut eth_pending = ETH_PENDING.borrow(cs).borrow_mut();
+        *eth_pending = true;
+    });
+
+    // Clear interrupt flags
+    let p = unsafe { Peripherals::steal() };
+    stm32_eth::eth_interrupt_handler(&p.ETHERNET_DMA);
 }
