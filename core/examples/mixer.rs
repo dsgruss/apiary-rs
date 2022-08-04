176021e747a5386fc1905c7170feb7bd9d795b97
Daniel Gruss <dsgruss@users.noreply.github.com>
2022-08-04 09:26:49 -0700

    Add level interpolation

--- core/examples/mixer.rs
+++ core/examples/mixer.rs
@@ -1,9 +1,11 @@
-use apiary_core::AudioPacket;
+use apiary_core::{AudioPacket, CHANNELS};
 use itertools::izip;
 
 use crate::display_module::{DisplayModule, Processor};
 
-pub struct Mixer;
+pub struct Mixer {
+    level: [[f32; 3]; CHANNELS],
+}
 
 const SCALE_PARAM: usize = 0;
 const NUM_PARAMS: usize = 1;
@@ -31,7 +33,9 @@ impl Mixer {
             .input(LEVEL2_INPUT, "Level 2")
             .param(SCALE_PARAM, 0.0, 100.0, 100.0, "Scale", "%", false)
             .output(MIX_OUTPUT, "Mix Out")
-            .start(Mixer {})
+            .start(Mixer {
+                level: [[0.0; 3]; 8],
+            })
     }
 }
 
@@ -51,18 +55,22 @@ impl Processor<NUM_INPUTS, NUM_OUTPUTS, NUM_PARAMS> for Mixer {
             input[LEVEL2_INPUT].data,
             output[MIX_OUTPUT].data.iter_mut()
         ) {
-            for (fin0, fl0, fin1, fl1, fin2, fl2, fo) in izip!(
+            for (fin0, fl0, fin1, fl1, fin2, fl2, fo, flev) in izip!(
                 in0.data,
                 l0.data,
                 in1.data,
                 l1.data,
                 in2.data,
                 l2.data,
-                o.data.iter_mut()
+                o.data.iter_mut(),
+                self.level.iter_mut(),
             ) {
-                *fo = ((fin0 as i32 * fl0 as i32 / 100 * params[SCALE_PARAM] as i32
-                    + fin1 as i32 * fl1 as i32 / 100 * params[SCALE_PARAM] as i32
-                    + fin2 as i32 * fl2 as i32 / 100 * params[SCALE_PARAM] as i32)
+                flev[0] += 0.01 * (fl0 as f32 - flev[0]);
+                flev[1] += 0.01 * (fl1 as f32 - flev[1]);
+                flev[2] += 0.01 * (fl2 as f32 - flev[2]);
+                *fo = ((fin0 as i32 * flev[0] as i32 / 100 * params[SCALE_PARAM] as i32
+                    + fin1 as i32 * flev[1] as i32 / 100 * params[SCALE_PARAM] as i32
+                    + fin2 as i32 * flev[2] as i32 / 100 * params[SCALE_PARAM] as i32)
                     >> 16) as i16;
             }
         }
